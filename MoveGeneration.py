6c39d95f6111b0f9cc1ab17a42101d11b591dea5
aabotaleb <aabotaleb@zewailcity.edu.eg>
2017-11-04 02:52:41 -0600

    Update MoveGeneration.py

--- MoveGeneration.py
+++ MoveGeneration.py
@@ -21,12 +21,53 @@ Implementation Team Work to test their code also [But read from console rather t
  get Move from GUI and feed it to doMove();
 
 """
+from collections import defaultdict
+import re
 
+### Debug Functions :
+#Display the bit board. [indicating the presence of a piece
+#into the console window for debug purposes
+# will be called one time for the white team and other time for the black team 
+# ( Abdelrhman )
+def displayBitBoard(BitBoard):
+  BitBoardTemp=BitBoard;
+  maskMax          =0xFFFFFFFFFFFFFF      #Mask which initilally will mask most 8th byte after show it   
+  for rowNum in range(7,-1,-1):           #Display Eight rows
+    BitBoardTemp= BitBoardTemp>>(8*rowNum)#Shift left the least bytes to display only one byte 
+    print(format(BitBoardTemp,'#010b'))   #Display the concerned byte in binary 
+    BitBoardTemp             = BitBoard & (maskMax)#Mask the most (rowNum+1)th Byte 
+    maskMax = maskMax >> 8                #Shift left the mask 1 byte 
+
+### Debug Functions :
+#Display the 32 array of integers showing the pieces positions
+#into the console window as (x,y) positions for debug purposes
+# ( Abdelrhman )
+def displayPieces(boardTemp):
+ print('White side:')                      #Print the white side positions  
+ for numLine in range(2):                  #Iterate for 2 rows 
+  for numElem in range(8):                 #Each row contains 8 elements 
+   piecePos=boardTemp[(8*numLine)+numElem];#get the corresponding decimal position from board  
+   if(piecePos!=-1): 					   #If piece not dead display its position 
+     print('('+str(int(piecePos/8))+' , '+str(piecePos%8)+')',end=" ")
+										   #X position from 0 to 7 and y position from 1 to 8 
+   else:
+     print('Dead   ',end=" ")              #Otherwise display dead 
+  print('');				
+ print('Black side:') 					   #Do the same for the black team [the remaining 16 elements] 
+ for numLine in range(2):
+  for numElem in range(8):
+   piecePos=boardTemp[16+(8*numLine)+numElem];
+   if(piecePos!=-1): 
+     print('('+str(int(piecePos/8))+' , '+str(piecePos%8)+')',end=" ")
+   else:
+     print('Dead   ',end=" ")  
+  print('')
+
+  
 #_____________________________________________________________________________#
 #This will convert the FEN file and covert it to a board
 #  return the array board[], BlackBitBoard , WhiteBitBoard
-from collections import defaultdict
-import re
+# ( Safa Tharwat and Saraa Ahmed )
 def readFEN2Board(FEN) :
     ranks = FEN.split(" ")[0].split("/")
     Side_To_M = FEN.split(" ")[1]
@@ -99,13 +140,15 @@ def readFEN2Board(FEN) :
     whitebitboard = ('{0:064b}'.format(w))
     return(board,blackbitboard,whitebitboard,C_A_L,E_P_T_S,H_M_Clk,F_M_Counter) 
 
-
+#Utility Function Used in CheckMate
+#Gamal Mohammed 
 def CheckAttackLine(BlackBitBoard,WhiteBitBoard,king_loc,step,board=[],potentialAttackers=[],board_limits=[]):
     i = king_loc + step
     esc = False
     while not esc:
         if i in board_limits:
             esc = True
+
         if WhiteBitBoard & (1 << i): # Hit white piece, I'm safe!
             break
         if BlackBitBoard & (1 << i): # Hit black piece, DANGER!!
@@ -114,7 +157,6 @@ def CheckAttackLine(BlackBitBoard,WhiteBitBoard,king_loc,step,board=[],potential
                     if p in potentialAttackers: 
                         return False
         i+=step
-        
     return True
 
 
@@ -122,7 +164,8 @@ def CheckAttackLine(BlackBitBoard,WhiteBitBoard,king_loc,step,board=[],potential
 #This will check if a move of a certain piece result in a check
 #   return true : if move will cause Check
 #   return false otherwise.
-def CheckMate(BlackBitBoard,WhiteBitBoard, move,castling, board=[], *args): # (Gamal Mohammed)
+# ( Gamal Mohammed )
+def CheckMate(BlackBitBoard,WhiteBitBoard, move,castling, board=[], *args):
     # Emulate move
     emulated_board = board[:]
     BlackBitBoard,WhiteBitBoard,emulated_board = doMove(BlackBitBoard,WhiteBitBoard,move,emulated_board)
@@ -173,47 +216,81 @@ def CheckMate(BlackBitBoard,WhiteBitBoard, move,castling, board=[], *args): # (G
     return True
 
 
-def generateMoves(BlackBitBoard,WhiteBitBoard,piece,left,right, board=[], *args): # (Hager) and (Abdelrhman)
-    possible = []
-    Chess_Chari = {"K": [-1, -1, -1, 0,  0,  1, 1, 1, 3,12],"B": [-1,  0, -1, 0,  0,  1, 0, 1, 1,10,14],"R": [0, -1, 0,  0,  0, 0, 1, 0, 1,8,15]}
-    Chess_Charj = {"K": [-1,  0,  1, -1, 1, -1, 0, 1, 3,12],"B": [-1,  0,  1, 0,  0, -1, 0, 1, 1,10,14],"R": [0,  0, 0, -1,  1, 0, 0, 0, 1,8,15]}
-    adi = Chess_Chari[piece]
-    adj = Chess_Charj[piece]
-    castling = [left,right]
-    castlingx = [136,9]
-    for j in range(2):
-        print(board[adi[9 + j]])
-        if board[adi[9 + j]] == -1:
-            print(adi)
-            continue
-        for i in range(8):
-            x = board[adi[9 + j]]
-            idxi = int(x / 8) + adi[i]
-            idxj = x % 8 + adj[i]
-            x = x + adi[i] * 8 + adj[i]
-            check = 1
-            while 0 <= idxi < 8 and 0 < idxj <= 8 and check == 1:
-                if x < 64:
-                    l = 1 << (64 - x)
-                if l & WhiteBitBoard > 0:
+def generateMoves(BlackBitBoard,WhiteBitBoard,piece,left,right,order, board =[], *args ): # (Hager) and (Abdelrhman)
+    possible=[]
+	#Chess_Chari is a dictionary (in form {'piece':adi} describes :
+	#a-   the possible x coordinate move : [one of available -max- 8 options] from adi[i] where 0<=i<=7 
+	#b-   the 'piece' corresponding index in board adi[9+j] where j corresponds to the jth replica of piece
+	#c-   adi[8] (=1 when piece can make its move many times , =2 if move done only one time , =3 in case of king)
+	
+	#order specify the order of pawn [from 0 to 7] , Bishop,Rook and Knight [0 or 1] 
+    
+    Chess_Chari={"K": [-1, -1, -1, 0,  0,  1, 1, 1,  3,12], \
+	"Q": [-1,-1,-1,0 ,0, 1,1,1,1, 11] ,"N": [-1,-2,-1,-2,1,2,1 ,2 ,2, 9,14],"P":[0,1,1,1,0,0,0,0,2, 0,1,2,3,4,5,6,7],\
+	"B": [-1,  0, -1, 0,  0,  1, 0, 1, 1, 10,13],"R": [ 0, -1, 0,  0,  0, 0, 1, 0, 1, 8,15]}
+	
+    Chess_Charj={"K": [-1,  0,  1, -1, 1, -1, 0, 1,  3,12], \
+	"Q": [-1, 0,1 ,-1,1,-1,0,1,1, 11] ,"N": [-2,-1,2 , 1,2,1,-2,-1,2, 9,14],"P":[0,0,1,-1,0,0,0,0,2, 0,1,2,3,4,5,6,7],\
+	"B": [-1,  0,  1, 0,  0, -1, 0, 1, 1, 10,13],"R": [ 0,  0, 0, -1,  1, 0, 0, 0, 1, 8,15]}
+
+    adi =Chess_Chari[piece]#get the vector adi corresponds to the piece 
+    adj =Chess_Charj[piece]#get the vector adj corresponds to the piece
+    
+    castling =[left,right]
+    castlingx=[136,9]
+
+    #Iterate over the 2 	
+	#Bishop ,knight and Rook has 2 positions in board [two types of bishops , knights and rooks]        
+    j=order;
+    if board[adi[9+j]]==-1:#If piece is dead then return None
+          return ;
+    x=board[adi[9+j]]      #Get old poistion of the piece  
+    print('Old position : '+str(int(x/8))+' , '+str(x%8))
+    if (piece == "P" and 8<x<=16): #If piece is pawn and this is the first move 
+      adi[0]=2;	                  #Then pawn can move 2 squares forward 
+    for i in range(8) :   
+	
+            x=board[adi[9+j]]    #Get old poistion of the piece  
+            idxi=int(x/8)+adi[i] #Make a proposed horizontal move : starts from 0 to 7 
+            idxj=x%8     +adj[i] #Make a proposed vertical   move : starts from 1 to 8 
+            x= x + adi[i]*8 + adj[i]
+            print('Proposed New position : ('+str(idxi)+' , '+str(idxj)+ ") = "+str(x))
+            check=1
+			
+            while 0 <= idxi <8 and 0 < idxj <=8 and check==1:
+                if x<64: #If x is valid then put proper value in l to check that no piece in proposed location
+                    l=1<<(64-x)
+                if l&WhiteBitBoard>0:#If piece exist then can't move forward 
                     break
-                u = piece + str(j) + " " + str(board[adi[8 + j]]) + " " + str(x)
-                check = adi[8]
-                if CheckMate(BlackBitBoard,WhiteBitBoard,u,False, board) == True:
+                if(piece=="P" and (i==2 or i==3)): #Pawn is in capturing move	
+                    if l&BlackBitBoard==0:#If no capture then no capturing move can be done 
+                        break
+                if(piece=="P" and (i==0 or i==1)): #Pawn is in not in capturing move	
+                    if l&BlackBitBoard>0:#If it try to capture prevent as Pawn can't capture in this way  
+                        break
+						
+                u=piece+str(j)+" "+str(board[adi[8+j]])+" "+ str(x)
+				
+                check=adi[8] #repeat the move if adi[8]==1 (Queen , Bishop and Rook only) 
+                if CheckMate(BlackBitBoard,WhiteBitBoard,u,False, board)==True: 
+                    print('New position : ('+str(idxi)+' , '+str(idxj)+ ") = "+str(x),end=" ")
                     possible.append(x)
-                if l & BlackBitBoard > 0:
+                if l&BlackBitBoard>0:#If piece exist then capture and make move [Need to be completed ] 
+                    print(' Capture Happens Here');
                     break
-                idxi = idxi + adi[i]
-                idxj = idxj + adj[i]
-                #print(str(idxi) +" "+ str(idxj))
-                x = x + adi[i] * 8 + adj[i]
-        if piece == "K":
-            break
+                print(" ")
+                idxi=idxi+adi[i]
+                idxj=idxj+adj[i]
+                #print(str(idxi) +"  "+ str(idxj))
+                x=x+adi[i]*8+adj[i]
+    
+			
     for j in range(2):
-        if adi[8] == 3 and castling[j] == 1:
-            if WhiteBitBoard ^ castlingx[j] == 0:
-                if CheckMate(BlackBitBoard,WhiteBitBoard,u,True, board) == True: #check 2 squares beside king and King
-                    possible.append((j + 1) * 1000)
+        if adi[8]==3 and castling[j]==1: #Happens with the King Only and if castling
+            if WhiteBitBoard^castlingx[j]==0:
+                if testCheck(BlackBitBoard,WhiteBitBoard,u,"true", board)=="true": #check 2 squares beside king and King
+                    possible.append((j+1)*1000)
+    
     return possible
 
 #_____________________________________________________________________________#
@@ -231,20 +308,3 @@ def doMove(BlackBitBoard,WhiteBitBoard, move, board=[], *args): #Hesham Magdy
 def updatePastMoves(move): #(Sara) and (Safa)
     return move
 
-
-#Bishop
-board = [-1,-1,-1,-1,-1,-1,-1,-1 ,-1,-1,37,-1,5,-1,-1,-1 ,-1,-1,-1,-1,-1,-1,-1,-1  ,-1,-1,-1,-1,-1,-1,-1,-1]
-BlackBitBoard = 1 << 18
-WhiteBitBoard = (1 << (64 - 37))
-#King
-#board=[-1,-1,-1,-1,-1,-1,-1,-1 ,-1,-1,-1,-1,61,-1,-1,-1
-#,-1,-1,-1,-1,-1,-1,-1,-1 ,-1,-1,-1,-1,-1,-1,-1,-1 ]
-#BlackBitBoard=0
-#WhiteBitBoard=9
-#Rook
-#board=[-1,-1,-1,-1,-1,-1,-1,-1 ,37,-1,-1,-1,-1,-1,-1,-1
-#,-1,-1,-1,-1,-1,-1,-1,-1 ,-1,-1,-1,-1,-1,-1,-1,-1]
-#BlackBitBoard=1<<18
-#WhiteBitBoard=(1<<(64-37))
-x = generateMoves(BlackBitBoard,WhiteBitBoard,"B",0,1, board)
-print(x)
